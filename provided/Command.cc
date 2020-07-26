4e181fa86ce361ddefb52f0c72c78dbc80480209
Julia Du <julia_du@outlook.com>
2020-07-26 16:32:50 -0400

    created source and header files for Command class

---
+++ provided/Command.cc
@@ -0,0 +1,5 @@
+//
+// Created by Julia Du on 2020-07-25.
+//
+
+#include "Command.h"
---
+++ provided/Command.h
@@ -0,0 +1,16 @@
+//
+// Created by Julia Du on 2020-07-25.
+//
+
+#ifndef PROJECT_2_QUADRIS_COMMAND_H
+#define PROJECT_2_QUADRIS_COMMAND_H
+
+
+
+class Command {
+
+};
+
+
+
+#endif //PROJECT_2_QUADRIS_COMMAND_H
