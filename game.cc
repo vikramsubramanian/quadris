964bdcd7424414100fa959518cabe135dd1a3f85
l3mohame <l3mohame@uwaterloo.ca>
2020-07-29 13:54:25 -0400

    Added "player.h" and "player.cc" from the strategy design pattern example, then renamed them to "game.h" and "game.cc".

---
+++ game.cc
@@ -0,0 +1,20 @@
+#include "player.h"
+#include "strategies.h"
+#include <memory>
+#include <utility>
+using namespace std;
+
+AgileBidder::AgileBidder(size_t chips) : 
+    strat{make_unique<Conservative>()}, chips{chips} {}
+
+size_t AgileBidder::bid() {
+    size_t bid = strat->bid(*this);
+    chips -= bid;
+    return bid;
+}
+size_t AgileBidder::chipCount() const {
+    return chips;
+}
+void AgileBidder::setStrategy(unique_ptr<BiddingStrategy> s) {
+    strat = move(s);
+}
---
+++ game.h
@@ -0,0 +1,16 @@
+#ifndef PLAYER_H_
+#define PLAYER_H_
+#include <memory>
+class BiddingStrategy;
+
+class AgileBidder {
+    std::unique_ptr<BiddingStrategy> strat;
+    size_t chips;
+  public:
+    AgileBidder(size_t);
+    size_t bid();
+    size_t chipCount() const;
+    void setStrategy(std::unique_ptr<BiddingStrategy>);
+};
+
+#endif
