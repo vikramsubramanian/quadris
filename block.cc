42de453f8e4a9de3524f2031f811c3c91211e6a7
Vikram N. Subramanian <vikram.subramanian6@gmail.com>
2020-08-02 16:43:54 -0400

    wip

--- block.cc
+++ block.cc
@@ -141,22 +141,21 @@ bool Block::translate(Direction dir, char board[18][11])
             break;
 
         case Direction::clockwise:
-            cout << "_____" << endl;
+
+            vector<vector<int>> newPos;
+            int lowestX =-1;
+            int lowestY =-1;
             for (int i = 0; i < pieceList.size(); i++)
             {
-                
                 //Matric multiplcation says rotating a coordinate (x,y) 90deg clockwise around a pivot (a,b)
                 //Produces (-y+a+b, -a+b+x)
                 newX = (-1 * pieceList.at(i).y) + rotateAroundPivot[0] + rotateAroundPivot[1];
                 newY = (pieceList.at(i).x) - rotateAroundPivot[0] + rotateAroundPivot[1] ;
-                cout << newX << "," << newY << endl;
-                if (newY < 0 || newY > 17 ||
-                    newX < 0 || newX > 11 ||
-                    board[newY][newX] != '_')
-                {
-                    cout << "FAILED!!!!" << endl;
-                    return false;
-                }
+                vector<int> temp;
+                temp.push_back(newX);
+                temp.push_back(newY);
+                newPos.push_back(temp);
+                // cout << newX << "," << newY << endl;
             }
 
             //If move is valid, we actually move the object
