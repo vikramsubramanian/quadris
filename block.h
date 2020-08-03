77730c9ac3d1d911a20717c46d1086458869ac14
l3mohame <l3mohame@uwaterloo.ca>
2020-08-03 04:19:21 -0400

    Overhauled the block code, greatly reducing it's complexity.

--- block.h
+++ block.h
@@ -6,20 +6,25 @@
 #include "piece.h"
 
 class Block{
+    private:
+        void _translate(bool, bool);
+        void _clockwise();
+        void _transform(Direction);
+        bool _inBounds(char board[18][11]);
 
-protected:
-    Block();
+    protected:
+        Block();
 
-public:
-    /* The constructor is protected so that it cannot be instantiated by anything other
-	 * than its factory. It's not private because subclasses need to call it.
-	 */
-    virtual ~Block() {};
-    std::vector<Piece> pieceList;
-    int rotateAroundPivot[2];
-    int level_;
-    void shiftDown(char board[18][11], int clearedRow);
-    bool translate(Direction, char board[18][11]);
+    public:
+        /* The constructor is protected so that it cannot be instantiated by anything other
+        * than its factory. It's not private because subclasses need to call it.
+        */
+        virtual ~Block() {};
+        std::vector<Piece> pieceList;
+        int rotateAroundPivot[2];
+        int level_;
+        void shiftDown(char board[18][11], int clearedRow);
+        bool translate(Direction, char board[18][11]);
     
 };
 
--- direction.h
+++ direction.h
@@ -1,6 +1,6 @@
 #ifndef DIRECTION_H_
 #define DIRECTION_H_
 
-enum Direction {left, right, down, clockwise, counterclockwise};
+enum Direction {left, right, up, down, clockwise, counterclockwise};
 
 #endif
