4fc262a41b578af9d8d4c1dde3983e765989259f
Vikram N. Subramanian <vikram.subramanian6@gmail.com>
2020-08-03 10:43:16 -0400

    Worked on block

--- block.cc
+++ block.cc
@@ -3,32 +3,37 @@
 #include <vector>
 using namespace std;
 
-Block::Block(){
-
-}
+Block::Block(){}
 
 void Block::shiftDown(char board[18][11], int clearedRow){
+    //This method is used to drop pieces when a row is cleared for the board.
+
     //Placeholder variables to store the location of the moved object
     int newX = -1;
     int newY = -1;
 
     // First we remove the block from this copy of the board
-
     for (int i = 0; i < pieceList.size(); i++)
     {
         board[pieceList.at(i).y][pieceList.at(i).x] = ' ';
     }
+
     for (int i = 0; i < pieceList.size(); i++)
     {
         newX = pieceList.at(i).x;
         newY = pieceList.at(i).y + 1;
-        //FIX THIS!
-        if (!(newY < 0 || newY > clearedRow ||
-              newX < 0 || newX > 11 ||
-              board[newY][newX] != ' '))
+
+        if ((newY < 0 || newY > clearedRow ||
+             newX < 0 || newX > 11         ||
+             board[newY][newX] != ' '))
+        {
+            
+        }
+        else
         {
             pieceList.at(i).y += 1;
         }
+        
     }
 }
 void Block::_translate(bool positive, bool horizontal) {
@@ -93,7 +98,9 @@ void Block::_clockwise() {
 }
 
 void Block::_transform(Direction dir)
-{    
+{
+    //Method to transform block's location without bounds checking
+    //We investigate the results of this method to see if the transform is valid
     switch (dir)
     {
         case Direction::left:
@@ -113,6 +120,7 @@ void Block::_transform(Direction dir)
             break;
 
         case Direction::counterclockwise:
+            // Counterclockwise is equivalent to calling clockwise 3 times
             _clockwise();
             _clockwise();
             _clockwise();
@@ -129,16 +137,23 @@ void Block::_transform(Direction dir)
 
 bool Block::_inBounds(char board[18][11])
 {
+    //This method checks if the block's location is valid/inbound
+    //Placeholder vars
     int newX, newY;
-
+    //We check every piece in the block.
     for (int i = 0; i < pieceList.size(); i++)
     {
         newX = pieceList.at(i).x;
         newY = pieceList.at(i).y;
+        //We check for 2 things-
+        //  1. If the piece lies within the playing area (within 0-11 columns and 0-17 rows )
+        //  2. If the piece occupies a position that is already occupied.
+
         if (newY < 0 || newY > 17 ||
             newX < 0 || newX >= 11 ||
             board[newY][newX] != ' ')
         {
+            //If even one piece fails the checks, the block cant be transformed
             return false;
         }
     }
@@ -149,15 +164,24 @@ bool Block::translate(Direction dir, char board[18][11])
 {    
     bool flag;
     // First we remove the block from this copy of the board
+    // One piece of a block, after transformation, could occupy another pieces location
+    //Therefore we, initially, remove the entire block we are trying to rotate.
     for (int i = 0; i < pieceList.size(); i++)
     {
         board[pieceList.at(i).y][pieceList.at(i).x] = ' ';
     }
-
+    //We do the transform and then check if it is valid in _inBounds()
+    //If the translate is not valid, flag turns false and the switch
+    // statement below runs
     _transform(dir);
     flag = _inBounds(board);
     
     if (!flag) {
+        //If this code runs, it means boundsChecking failed for transform 
+        //and we cant succesfully make the transformation
+        //So we undo our action by runnning translate in the
+        // opposite direction
+        //This switch calculates what that opposite direction is
         switch (dir) {
             case Direction::left:
                 dir = Direction::right;
@@ -186,220 +210,10 @@ bool Block::translate(Direction dir, char board[18][11])
             default:
                 break; 
         }
+        //We perform translate in the opposite direction
         _transform(dir);
     }
 
     return flag;
 }
 
-/*
-bool Block::translate(Direction dir, char board[18][11])
-{
-    //Placeholder variables to store the location of the moved object
-    int newX = -1;
-    int newY = -1;
-    
-    // First we remove the block from this copy of the board
-    for (int i = 0; i < pieceList.size(); i++)
-    {
-        board[pieceList.at(i).y][pieceList.at(i).x] = ' ';
-    }
-
-    int currentX = 12;
-    int currentY = -1;
-    int newXPos = 12;
-    int newYPos = -1;
-    int translateX = 12;
-    int translateY = -1;
-    vector<vector<int>> rotatedPos;
-    vector<int> temp;
-    switch (dir)
-    {
-        case Direction::left:
-            //we check if move is valid for all sub pieces in the object
-            for (int i = 0; i < pieceList.size(); i++)
-            {
-                newX = pieceList.at(i).x - 1;
-                newY = pieceList.at(i).y;
-                if(newY < 0 || newY > 17  ||
-                newX < 0  || newX >= 11  ||
-                board[newY][newX] != ' ')
-                {
-
-                    return false;
-                }
-            }
-            //If move is valid, we actually move the object
-            for (int i = 0; i < pieceList.size(); i++)
-            {
-                pieceList.at(i).x -= 1;
-            }
-            rotateAroundPivot[0] -= 1;
-            break;
-
-        case Direction::right:
-
-            for (int i = 0; i < pieceList.size(); i++)
-            {
-                newX = pieceList.at(i).x + 1;
-                newY = pieceList.at(i).y;
-                if (newY < 0 || newY > 17 ||
-                    newX < 0 || newX >= 11 ||
-                    board[newY][newX] != ' ')
-                {
-                    return false;
-                }
-            }
-
-            //If move is valid, we actually move the object
-            for (int i = 0; i < pieceList.size(); i++)
-            {
-                pieceList.at(i).x += 1;
-            }
-            rotateAroundPivot[0] += 1;
-            break;
-        case Direction::down:
-            for (int i = 0; i < pieceList.size(); i++)
-            {
-                newX = pieceList.at(i).x;
-                newY = pieceList.at(i).y + 1;
-                if (newY < 0 || newY > 17 ||
-                    newX < 0 || newX >= 11 ||
-                    board[newY][newX] != ' ')
-                {
-                    return false;
-                }
-            }
-
-            //If move is valid, we actually move the object
-            for (int i = 0; i < pieceList.size(); i++)
-            {
-                pieceList.at(i).y += 1;
-            }
-            rotateAroundPivot[1] += 1;
-
-            break;
-
-        case Direction::counterclockwise:
-
-            cout << "comng here" << endl;
-            for (int i = 0; i < pieceList.size(); i++)
-            {
-                if (pieceList.at(i).x < currentX)
-                {
-                    currentX = pieceList.at(i).x;
-                }
-                if (pieceList.at(i).y > currentY)
-                {
-                    currentY = pieceList.at(i).y;
-                }
-            }
-
-            rotatedPos.clear();
-            for (int i = 0; i < pieceList.size(); i++)
-            {
-                temp.clear();
-                temp.push_back(pieceList.at(i).y);
-                temp.push_back((-1 * pieceList.at(i).x));
-                rotatedPos.push_back(temp);
-            }
-
-            for (int i = 0; i < rotatedPos.size(); i++)
-            {
-                if (rotatedPos.at(i).at(0) < newXPos)
-                {
-                    newXPos = rotatedPos.at(i).at(0);
-                }
-                if (rotatedPos.at(i).at(1) > newYPos)
-                {
-                    newYPos = rotatedPos.at(i).at(1);
-                }
-            }
-            translateX = currentX - newXPos;
-            translateY = currentY - newYPos;
-            cout << "current xy:" << currentX << "," << currentY << endl;
-            cout << "new xy:" << newXPos << "," << newYPos << endl;
-            for (int i = 0; i < rotatedPos.size(); i++)
-            {
-                newX = rotatedPos.at(i).at(0) + translateX;
-                newY = rotatedPos.at(i).at(1) + translateY;
-                if (newY < 0 || newY > 17 ||
-                    newX < 0 || newX >= 11 ||
-                    board[newY][newX] != ' ')
-                {
-                    return false;
-                }
-            }
-            for (int i = 0; i < rotatedPos.size(); i++)
-            {
-                pieceList.at(i).x = rotatedPos.at(i).at(0) + translateX;
-                pieceList.at(i).y = rotatedPos.at(i).at(1) + translateY;
-                cout << "New position" << pieceList.at(i).x << "," << pieceList.at(i).y << endl;
-            }
-
-            break;
-
-        case Direction::clockwise:
-            cout << "comng here" << endl;
-            for (int i = 0; i < pieceList.size(); i++)
-            {
-                if (pieceList.at(i).x < currentX)
-                {
-                    currentX = pieceList.at(i).x;
-                }
-                if (pieceList.at(i).y > currentY)
-                {
-                    currentY = pieceList.at(i).y;
-                }
-            }
-
-            rotatedPos.clear();
-            for (int i = 0; i < pieceList.size(); i++)
-            {
-                temp.clear();
-                temp.push_back(-1 * pieceList.at(i).y);
-                temp.push_back((pieceList.at(i).x));
-                rotatedPos.push_back(temp);
-            }
-
-            for (int i = 0; i < rotatedPos.size(); i++)
-            {
-                if (rotatedPos.at(i).at(0) < newXPos)
-                {
-                    newXPos = rotatedPos.at(i).at(0);
-                }
-                if (rotatedPos.at(i).at(1) > newYPos)
-                {
-                    newYPos = rotatedPos.at(i).at(1);
-                }
-            }
-            translateX = currentX - newXPos;
-            translateY = currentY - newYPos;
-            cout << "current xy:" << currentX << "," << currentY << endl;
-            cout << "new xy:" << newXPos << "," << newYPos << endl;
-            for (int i = 0; i < rotatedPos.size(); i++)
-            {
-                newX = rotatedPos.at(i).at(0) + translateX;
-                newY = rotatedPos.at(i).at(1) + translateY;
-                if (newY < 0 || newY > 17 ||
-                    newX < 0 || newX >= 11 ||
-                    board[newY][newX] != ' ')
-                {
-                    return false;
-                }
-            }
-            for (int i = 0; i < rotatedPos.size(); i++)
-            {
-                pieceList.at(i).x = rotatedPos.at(i).at(0) + translateX;
-                pieceList.at(i).y = rotatedPos.at(i).at(1) + translateY;
-                cout << "New position" << pieceList.at(i).x << "," << pieceList.at(i).y << endl;
-            }
-
-            break;
-        default:
-            break;
-        
-    }
-    return true;
-}
-*/
